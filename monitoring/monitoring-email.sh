ff3cb0b1742e9c9e5ae4c73236b199b7009a3459
Roman <roman.quistler@hotmail.de>
2017-10-11 12:46:53 +0200

    monitoring

--- monitoring/monitoring-email.sh
+++ monitoring/monitoring-email.sh
@@ -4,14 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-source borg-passphrase.sh
+source /usr/local/sbin/borg-passphrase.sh
 
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
 export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
 
-readonly USER=roman
-readonly HOST=kroon.fi
-readonly REPO=/mnt/borealis/ensumer/home/backup # Path to repository on the host
+readonly USER="roman"
+readonly HOST="kroon.fi"
+readonly REPO="/mnt/borealis/ensumer/home/backup" # Path to repository on the host
 readonly TARGET="${USER}@${HOST}/${REPO}"
 readonly BORG_CMD_REMOTE_PATH="/mnt/borealis/ensumer/home/borg-linux64"
 
@@ -19,7 +19,9 @@ readonly DOMAINS_DIR="/srv/http/domain/"
 readonly EMAIL_ADDRESS="server@7nerds.de"
 declare -A DOMAINS_STATUS
 
-DOMAINS=("7nerds.de")
+DOMAINS=("7nerds.de" "ewqewqe21.de")
+send_mail=false
+was_backup_successful=true
 
 
 ## last five backups
@@ -40,30 +42,50 @@ get_domains_in_dir(){
       DOMAINS+=("${DIR_WITHOUT_SLASH##*/}")
     fi
   done
-
-  print_array DOMAINS[@]
 }
 
 
 check_if_websites_are_online(){
 
-  local OUTPUT
-
   for domain in "${DOMAINS[@]}"
   do
-    OUTPUT="$(curl -sSf -m 5 ${domain} 2>&1)"
 
-    if [[ ! -z "${OUTPUT}" ]]; then
-      echo "hi, I am not empty"
+    if wget --server-response --timeout=3 --tries=1 --spider "${domain}" 2>&1 | grep -q "200 OK"; then
+      DOMAINS_STATUS["${domain}"]="Online"
+    else
+      DOMAINS_STATUS["${domain}"]="Offline"
+      send_email=true
     fi
 
   done
 
+  for i in "${!DOMAINS_STATUS[@]}"
+  do
+    echo "key  : $i"
+    echo "value: ${DOMAINS_STATUS[$i]}"
+  done
 }
+
+check_if_backup_was_successful(){
+
+
+  if systemctl status backup.service | grep -q "status=0/SUCCESS"; then
+    was_backup_successful=true
+  else
+    was_backup_successful=false
+  fi
+
+  echo "${was_backup_successful}"
+
+}
+
 
 send_email(){
 
-  echo $(print_status) | mailx -s "status" "${EMAIL_ADDRESS}"
+  if [ "$send_email" = true ] ; then
+    ## TODO
+    echo $(print_status) | mailx -s "status" "${EMAIL_ADDRESS}"
+  fi
 }
 
 print_status(){
@@ -78,3 +100,4 @@ print_array(){
 
 get_domains_in_dir
 check_if_websites_are_online
+check_if_backup_was_successful
