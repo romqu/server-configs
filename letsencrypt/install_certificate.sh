b8f08ff4227799c5fd8cb2702a720e5ec86a6ad2
Roman <roman.quistler@hotmail.de>
2017-10-30 13:21:34 +0100

    acme script

--- letsencrypt/install_certificate.sh
+++ letsencrypt/install_certificate.sh
@@ -8,19 +8,19 @@ set -o nounset
 readonly domains=("$1" "www.$1")
 readonly certificate_dir="/etc/ssl/private/"
 readonly domain_dir="/srv/http/domain/"
-
+readonly acme_cmd="/root/.acme.sh/acme.sh"
 
 install_certificates(){
 
-  for domain in domains; do
+  for domain in "${domains[@]}"; do
 
     if [ ! -d "${certificate_dir}${domain}" ]; then
       mkdir "${certificate_dir}${domain}"
     fi
 
-    acme.sh --issue -d "${domain}" -w "${domain_dir}${domain}" --nginx --debug --keylength ec-384
+    "${acme_cmd}" --issue -d "${domain}" -w "${domain_dir}${domain}" --nginx --debug --keylength ec-384
 
-    acme.sh --install-cert --ecc --debug -d "${domain}" \
+    "${acme_cmd}" --install-cert --ecc --debug -d "${domain}" \
     --cert-file      /etc/ssl/private/rodnok.de/"${domain}".cert.pem  \
     --key-file       /etc/ssl/private/rodnok.de/"${domain}".key.pem  \
     --fullchain-file /etc/ssl/private/rodnok.de/fullchain.pem \
@@ -30,3 +30,5 @@ install_certificates(){
 
   systemctl reload nginx.service
 }
+
+install_certificates
